f7e9ac24f76b813e0217604ea3c4970de0e0ad29
David Cary <d.cary@ieee.org>
2025-02-21 16:20:58 -0800

    starting Morse code project

---
+++ morse.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+morse.py
+Morse code translater
+Created on Fri Feb 21 15:16:13 2025
+
+@author: dc
+
+2025-02-21: started by David Cary.
+
+FUTURE: automatically detect
+    * ... --- ... dots and dashes
+    * - - -   --- --- ---   - - - long (dashes) vs short (dots)
+    * Normal ASCII text to translate into Morse
+"""
+# ---- TOF
+
+
+# ---- imports
+
+
+# ---- code
+
+
+
+
+# ---- EOF
