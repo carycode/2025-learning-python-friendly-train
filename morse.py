777935a4e86f573c3958ecb31669a8863df56a61
David Cary <d.cary@ieee.org>
2025-02-23 12:32:53 -0800

    fill in details, with official ITU reference.

--- morse.py
+++ morse.py
@@ -12,6 +12,13 @@ FUTURE: automatically detect
     * ... --- ... dots and dashes
     * - - -   --- --- ---   - - - long (dashes) vs short (dots)
     * Normal ASCII text to translate into Morse
+
+FUTURE:
+    handle standard Morse code abbreviations and prosigns
+    https://en.wikipedia.org/wiki/Morse_code_abbreviations
+    https://en.wikipedia.org/wiki/Prosigns_for_Morse_code
+    such as
+    73 	Best regards
 """
 # ---- TOF
 
@@ -21,7 +28,25 @@ FUTURE: automatically detect
 
 # ---- code
 
+def build_morse_dictionary(){
+        
+    # p. 2 of
+    # https://www.itu.int/dms_pubrec/itu-r/rec/m/R-REC-M.1677-1-200910-I!!PDF-E.pdf
+    morse_letters = {
+        'a': '.-',
+        'b': '-...',
+        'c': '-.-.',
+        
+        
+        }
+    morse_digits = {
+        }
+    # p. 3 of
+    # https://www.itu.int/dms_pubrec/itu-r/rec/m/R-REC-M.1677-1-200910-I!!PDF-E.pdf
+    morse_punctuation = {
+        }
 
+}
 
 
 # ---- EOF
