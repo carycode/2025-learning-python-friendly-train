e70f0f601bf6dcfe45d7d3bfc1f70c5d843bceb6
David Cary <d.cary@ieee.org>
2025-02-27 11:27:01 -0800

    setting up morse-to-text translation...

--- morse.py
+++ morse.py
@@ -7,12 +7,15 @@ Created on Fri Feb 21 15:16:13 2025
 
 @author: dc
 
+2025-02-26: DAV: fill in some comments.
+2025-02-24: A little help by Russ Hensel: text-to-dots-and-dashes now works
 2025-02-21: started by David Cary.
 
 FUTURE: automatically detect
     * ... --- ... dots and dashes
     * - - -   --- --- ---   - - - long (dashes) vs short (dots)
     * Normal ASCII text to translate into Morse
+    * slashes between words (common on internet); or long gaps between words
 
 FUTURE:
     handle standard Morse code abbreviations and prosigns
@@ -33,6 +36,8 @@ https://peps.python.org/pep-0263/
 *seems* to say first or second line is
     # -*- coding: utf-8 -*-
 As recommended by
+https://docs.python.org/3/tutorial/appendix.html#executable-python-scripts
+and
 https://stackoverflow.com/questions/6908143/should-i-put-shebang-in-python-scripts-and-what-form-should-it-take
 the first line is the recommended shebang line
     #!/usr/bin/env python3
@@ -43,6 +48,21 @@ However,
 https://peps.python.org/pep-0008/
 suggests
 "Code ... should always use UTF-8, and should not have an encoding declaration."
+
+Related:
+    
+* Morse code decoder https://imgur.com/gallery/code-Ydr0xky
+* Morse code chart https://www.reddit.com/r/codes/comments/jvmtdk/morse_code_chart_very_useful_resource/
+* learn cw online (LCWO): https://lcwo.net 
+* online Morse code training tools and history https://morsecode.world/
+* looking for app to listen to Morse code and decode it https://www.reddit.com/r/ios/comments/66mj69/app_that_can_listen_to_morse_code_and_translate_it/
+* "If only the camera could read Morse code" https://www.reddit.com/r/shortcuts/comments/9hj2ol/morse_code_shortcut_that_translates_text_into/
+* "Morse code Generator in Python" https://www.reddit.com/r/learnpython/comments/kr3yr9/morse_code_generator_in_python/
+* "Neat site for learning Morse Code" https://www.reddit.com/r/amateurradio/comments/1cxag6t/neat_site_for_learning_morse_codethats_mostly/
+* "I made a Morse code translator" (text-to-sound) https://www.reddit.com/r/Python/comments/w5balg/i_made_a_morse_code_translator/
+* text-to-morse (great documentation) https://github.com/Kishanjaisoorya/Morse-code-convertor-python
+* "How to decode morse code in a more pythonic way" https://stackoverflow.com/questions/71099952/how-to-decode-morse-code-in-a-more-pythonic-way
+* "Morse Code Ninja - A complete Morse Code Learning Tool" https://www.reddit.com/r/amateurradio/comments/wqow80/morse_code_ninja_a_complete_morse_code_learning/
 
 """
 # ---- TOF
@@ -109,6 +129,11 @@ def build_morse_dictionary():
         '@': '.--.-.'
         }
     
+    # FUTURE: perhaps add the prosigns and abbreviations from
+    # https://morsecode.world/international/morse.html
+    # https://www.radioqth.net/morsecode
+    # ?
+    
     # misc other stuff not exactly called out
     morse_other = {
         ' ': '  ' # two spaces here, so we end up with a net 3 spaces between words
@@ -121,18 +146,29 @@ def build_morse_dictionary():
     translation_table.update(morse_other)
     
     reverse_translation_table = {}
-    # FIXME
+    for key,value in translation_table.items():
+        reverse_translation_table[value] = key
+
+    # check for duplicates
+    for key,value in translation_table.items():
+        reverse_value = reverse_translation_table[value]
+        if key != reverse_value:
+            print( f"whoops: Morse {value} mapped to both {key} and {reverse_value} ")
+        
+    # check for duplicates
+    #assert len(reverse_translation_table) == len(translation_table)
     
     return translation_table, reverse_translation_table
 
 
+
 def main():
-        table, reverse_table = build_morse_dictionary()
-        print( table )
+        text_to_morse, morse_to_text = build_morse_dictionary()
+        print( text_to_morse )
         text = "Testing Morse with David and Russ"
         output = []
         for character in text:
-            temp = table[character.lower()] + ' ' # single space between letters
+            temp = text_to_morse[character.lower()] + ' ' # single space between letters
             print( f"\n({ temp = })")
             # output += temp # splits up strings into letters ...
             output.append(temp) # appends the string as a single item
