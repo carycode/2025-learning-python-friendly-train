aaa39e09eb317f1c06986e87a343c4285a69cd55
David Cary <d.cary@ieee.org>
2025-03-03 18:01:10 -0800

    no real change -- remove trailing whitespace.

--- morse.py
+++ morse.py
@@ -23,7 +23,7 @@ FUTURE:
     https://en.wikipedia.org/wiki/Prosigns_for_Morse_code
     such as
     73 	Best regards
-    
+
 FUTURE:
     consider making sounds
     consider listening to sounds
@@ -50,10 +50,10 @@ suggests
 "Code ... should always use UTF-8, and should not have an encoding declaration."
 
 Related:
-    
+
 * Morse code decoder https://imgur.com/gallery/code-Ydr0xky
 * Morse code chart https://www.reddit.com/r/codes/comments/jvmtdk/morse_code_chart_very_useful_resource/
-* learn cw online (LCWO): https://lcwo.net 
+* learn cw online (LCWO): https://lcwo.net
 * online Morse code training tools and history https://morsecode.world/
 * looking for app to listen to Morse code and decode it https://www.reddit.com/r/ios/comments/66mj69/app_that_can_listen_to_morse_code_and_translate_it/
 * "If only the camera could read Morse code" https://www.reddit.com/r/shortcuts/comments/9hj2ol/morse_code_shortcut_that_translates_text_into/
@@ -74,7 +74,7 @@ Related:
 # ---- code
 
 def build_morse_dictionary():
-        
+    
     # p. 2 of
     # https://www.itu.int/dms_pubrec/itu-r/rec/m/R-REC-M.1677-1-200910-I!!PDF-E.pdf
     morse_letters = {
@@ -87,7 +87,7 @@ def build_morse_dictionary():
         'f': '..-.',
         'g': '--.',
         'h': '....',
-        
+
         'i': '..',
         'j': '.---',
         'k': '-.-',
@@ -140,12 +140,12 @@ def build_morse_dictionary():
         #...
         '@': '.--.-.'
         }
-    
+
     # FUTURE: perhaps add the prosigns and abbreviations from
     # https://morsecode.world/international/morse.html
     # https://www.radioqth.net/morsecode
     # ?
-    
+
     # misc other stuff not exactly called out
     morse_other = {
         ' ': '  ' # two spaces here, so we end up with a net 3 spaces between words
@@ -156,7 +156,7 @@ def build_morse_dictionary():
     translation_table.update(morse_digits)
     translation_table.update(morse_punctuation)
     translation_table.update(morse_other)
-    
+
     reverse_translation_table = {}
     for key,value in translation_table.items():
         reverse_translation_table[value] = key
@@ -168,7 +168,7 @@ def build_morse_dictionary():
             print( f"whoops: Morse {value} mapped to both {key} and {reverse_value} ")
     # check for duplicates
     assert len(reverse_translation_table) == len(translation_table)
-    
+
     return translation_table, reverse_translation_table
 
 
